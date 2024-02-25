154677445047b7f29e5510aa52093b4672dcb1be
Dan Barr <dan.barr@outlook.com>
2024-02-25 12:18:59 -0500

    exercises 1.8 and 1.9

--- ch1/1.5/fetch.go
+++ ch1/1.5/fetch.go
@@ -5,10 +5,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -21,6 +26,6 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%d", beep)
+		fmt.Printf("%d %d", beep, resp.StatusCode)
 	}
 }
